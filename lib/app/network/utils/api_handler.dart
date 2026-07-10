481813e556972cd7a996289a1a1017ff39796034
dev-junaid-khan <junaid@youngengineers.org>
2026-07-10 11:55:59 +0530

    Updated the release version 1.0.1+5

--- lib/app/network/utils/api_handler.dart
+++ lib/app/network/utils/api_handler.dart
@@ -223,6 +223,8 @@ class ApiHandler {
                 : NoInternetConnectionException(
                     errorMessage: exceptionMessages.noInternetConnection,
                   ),
+            // TODO: Handle this case.
+            DioExceptionType.transformTimeout => throw UnimplementedError(),
           };
           return networkException;
         }
